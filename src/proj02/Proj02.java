2d21cb3794923218441c9561f03f988541080e65
suporte <suporte@172.16.1.119>
2020-02-18 20:24:39 -0300

    Primeiro commit

---
+++ src/proj02/Proj02.java
@@ -0,0 +1,13 @@
+
+package proj02;
+
+
+public class Proj02 {
+
+   
+    public static void main(String[] args) {
+       Tela tela = new Tela();
+       tela.setVisible(true);
+    }
+    
+}
---
+++ src/proj02/Teste.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proj02;
+
+import entidades.Aluno;
+import servico.CalcularMedia;
+
+/**
+ *
+ * @author suporte
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Aluno aluno = new Aluno();
+         aluno.setNome("Maria");
+         aluno.setNota1(8);
+         aluno.setNota2(5);
+         aluno.setMedia(CalcularMedia.calcularMedia(aluno.getNota1(), aluno.getNota2()));
+         aluno.setSituacao(CalcularMedia.situacaoAluno(aluno.getMedia()));
+         
+         System.out.println("O aluno: " + aluno.getNome() + 
+                 " com a media: " + aluno.getMedia() + 
+                 " foi " + aluno.getSituacao());
+    }
+    
+}
